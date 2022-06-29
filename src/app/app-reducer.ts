b0af997afc9a891cfb6558d9c845bfa5da32fcfe
DimaDeg <ddeg1809@gmail.com>
2022-06-29 19:10:57 +0300

    add redux toolkit

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,65 @@
+import {Dispatch} from "redux"
+import {authAPI} from "../api/todolists-api"
+import {setIsLoggedInAC} from "../features/Login/auth-reducer";
+import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState: InitialStateType = {
+    status: 'idle',
+    error: null,
+    isInitialized: false
+}
+
+const slice = createSlice({
+    name: 'app',
+    initialState: initialState,
+    reducers: {
+        setAppErrorAC: (state, action: PayloadAction<{ error: string | null }>) => {
+            state.error = action.payload.error
+        },
+        setAppStatusAC: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+            state.status = action.payload.status
+        },
+        SetAppIsInitializedAC:(state, action: PayloadAction<{ isInitialized: boolean }>)=> {
+            state.isInitialized = action.payload.isInitialized
+        }
+    }
+})
+
+export const appReducer = slice.reducer;
+export const {SetAppIsInitializedAC, setAppStatusAC, setAppErrorAC} = slice.actions;
+
+
+//types
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null
+    isInitialized: boolean
+}
+
+
+//thunks
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC({status: 'loading'}))
+    authAPI.me().then(res => {
+        debugger
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC({value: true}));
+            dispatch(setAppStatusAC({status: 'succeeded'}))
+        } else {
+            handleServerAppError(res.data, dispatch)
+            dispatch(setAppStatusAC({status: 'failed'}))
+        }
+    }).catch((error) => {
+        handleServerNetworkError(error, dispatch)
+        dispatch(setAppStatusAC({status: 'failed'}))
+    }).finally(() => {
+        dispatch(SetAppIsInitializedAC({isInitialized: true}))
+        dispatch(setAppStatusAC({status: 'failed'}))
+    })
+}
+
+
